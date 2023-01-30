9844df1aee46056ea15232a2130a7bde6bfd15c5
slow-codex <arnabduttasocial@gmail.com>
2023-01-30 18:40:21 +0530

    Chatting Back End

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,7 @@
-const host = "http://localhost:5000";
-export const registerRoute = "${host}/api/auth/register";
+export const host = "http://localhost:5000";
 export const loginRoute = `${host}/api/auth/login`;
-export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const registerRoute = `${host}/api/auth/register`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
+export const setAvatarRoute = `${host}/api/auth/setavatar`;
